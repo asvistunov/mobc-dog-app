bb53acccf3ebebfb75db6c3ab49baade71d32b77
asvistunov <aleksandrsvistunov4@gmail.com>
2023-11-15 21:01:16 +0300

    Changed json structure, main script and readme

--- main.py
+++ main.py
@@ -30,48 +30,50 @@ with open('app_urls.json', 'r') as app_urls_file:
     app_urls = json.load(app_urls_file)
 
 
-for app in app_urls:
+for app in app_urls['students_apps']:
 
     testing_images = {}
-    for url_type in app_urls[app]:
+
+    for url_type in ['github', 'url']:
+
         logger.info(f'Started building testing image for {url_type}.')
         testing_image, _ = client.images.build(
             path='docker/app-testing/',
             tag=f'{url_type}_clinic-app-testing',
                 buildargs={
-                'APP_URL': app_urls[app][url_type] if url_type == 'url' else 'http://clinic-app:80'
+                'APP_URL': app[url_type] if url_type == 'url' else 'http://clinic-app:80'
             }
         )
         logger.info(f'Finished building testing image for {url_type}.')
 
         testing_images[url_type] = testing_image
 
-    logger.info(f'Started building app image from {app_urls[app]["github"]}')
+    logger.info(f'Started building app image from {app["github"]}')
+
     app_image, _ = client.images.build(
         path='docker/app-hosting/',
         buildargs={
-            'GITHUB_REPO_URL': app_urls[app]['github']
+            'GITHUB_REPO_URL': app['github']
         },
         tag='clinic-app'
     )
     logger.info(f'Finished building app image')
 
-    logger.info(f'Running app image from {app_urls[app]["github"]}')
     app_container = client.containers.run(
         app_image.id,
         network=network.id, 
         name='clinic-app',
         detach=True
     )
-    logger.info(f'Running app image from {app_urls[app]["github"]}')
+    logger.info(f'Running app image from {app["github"]}')
 
     for image_type in testing_images:
 
         try:
-            logger.info(f'Started tests for {app} {image_type} image')
+            logger.info(f'Started tests for {app["student_id"]} {image_type} image')
             client.containers.run(
                 testing_images[image_type].id,
-                command=f'pytest /mobc-dog-clinic-testing/api_testing.py --no-header --no-summary  -q --json-report --json-report-file /mobc-dog-clinic-testing/results/{app}/{image_type}-report.json --json-report-omit warnings',
+                command=f'pytest /mobc-dog-clinic-testing/api_testing.py --no-header --no-summary  -q --json-report --json-report-file /mobc-dog-clinic-testing/results/{app["student_id"]}/{image_type}-report.json --json-report-omit warnings',
                 network=network.id,
                 name='clinic-app-testing',
                 mounts=[mount],
